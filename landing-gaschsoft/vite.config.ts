e35743bd13ed2132f2c0a4d8ed35244b05bd1b52
aschaadgt <luisangelschaad@gmail.com>
2025-04-19 05:22:57 -0600

    Fix: Ajuste vite.config.ts para deploy en root

--- landing-gaschsoft/vite.config.ts
+++ landing-gaschsoft/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing-gaschsoft/', // 👈 MUY IMPORTANTE colocar el nombre del repositorio aquí
 })
