04eb24862735d9aa7f6179d04762b1cacc0c2f6d
aschaadgt <luisangelschaad@gmail.com>
2025-04-20 03:21:39 -0600

    Deploy: ActualizaciÃ³n automÃ¡tica del landing Gaschsoft

--- landing-gaschsoft/src/components/GallerySection.tsx
+++ landing-gaschsoft/src/components/GallerySection.tsx
@@ -7,15 +7,15 @@ function GallerySection() {
         <div className="flex flex-1 gap-6 min-w-[300px]">
           {/* Imagen Vertical */}
           <div className="flex-1 rounded-3xl overflow-hidden shadow-xl">
-            <img src="/Vertical1.webp" alt="Imagen Vertical 1" className="w-full h-full object-cover" />
+            <img src="/Vertical1.png" alt="Imagen Vertical 1" className="w-full h-full object-cover" />
           </div>
           {/* Columnita de dos horizontales */}
           <div className="flex flex-col flex-1 gap-6">
             <div className="flex-1 rounded-3xl overflow-hidden shadow-xl">
-              <img src="/Horizontal1.webp" alt="Imagen Horizontal 1" className="w-full h-full object-cover" />
+              <img src="/Horizontal1.png" alt="Imagen Horizontal 1" className="w-full h-full object-cover" />
             </div>
             <div className="flex-1 rounded-3xl overflow-hidden shadow-xl">
-              <img src="/Horizontal2.webp" alt="Imagen Horizontal 2" className="w-full h-full object-cover" />
+              <img src="/Horizontal2.png" alt="Imagen Horizontal 2" className="w-full h-full object-cover" />
             </div>
           </div>
         </div>
@@ -24,15 +24,15 @@ function GallerySection() {
         <div className="flex flex-1 gap-6 min-w-[300px]">
           {/* Imagen Vertical */}
           <div className="flex-1 rounded-3xl overflow-hidden shadow-xl">
-            <img src="/Vertical2.webp" alt="Imagen Vertical 2" className="w-full h-full object-cover" />
+            <img src="/Vertical2.png" alt="Imagen Vertical 2" className="w-full h-full object-cover" />
           </div>
           {/* Columnita de dos horizontales */}
           <div className="flex flex-col flex-1 gap-6">
             <div className="flex-1 rounded-3xl overflow-hidden shadow-xl">
-              <img src="/Horizontal3.webp" alt="Imagen Horizontal 3" className="w-full h-full object-cover" />
+              <img src="/Horizontal3.png" alt="Imagen Horizontal 3" className="w-full h-full object-cover" />
             </div>
             <div className="flex-1 rounded-3xl overflow-hidden shadow-xl">
-              <img src="/Horizontal4.webp" alt="Imagen Horizontal 4" className="w-full h-full object-cover" />
+              <img src="/Horizontal4.png" alt="Imagen Horizontal 4" className="w-full h-full object-cover" />
             </div>
           </div>
         </div>
