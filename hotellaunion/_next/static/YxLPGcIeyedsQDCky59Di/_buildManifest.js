ac06e3de80a7842d05ea1fe42b325d63d6b5a654
aschaadgt <183251816+aschaadgt@users.noreply.github.com>
2026-07-18 17:25:06 -0600

    feat: publish Hotel La Union website

---
+++ hotellaunion/_next/static/YxLPGcIeyedsQDCky59Di/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hotellaunion//_next/:path+",
+        "destination": "/hotellaunion/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
