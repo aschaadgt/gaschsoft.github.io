3c9def1ba6b3881d22d2d8327298fa5939307ce2
aschaadgt <183251816+aschaadgt@users.noreply.github.com>
2026-09-12 15:19:55 -0600

    [Fix] GaschSoft suaviza transición entre Casos y Contacto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gaschsoft-shell-v7";
+const CACHE_NAME = "gaschsoft-shell-v8";
 const APP_SHELL = [
   "/", "/index.html", "/site.webmanifest",
   "/plataforma/", "/plataforma/desarrollo-web/", "/plataforma/automatizaciones/",
